321f3367fbc59652b136fa22e7b5a82ff0d68185
Prashant <prashant.stha246810@gmail.com>
2022-04-25 17:42:09 +0545

    nc port scanner

---
+++ nc_port.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+hit_C () {
+
+printf "\rYikes! Aborting!!" | lolcat
+exit
+}
+
+trap "hit_C" 2
+
+
+if [[ $1 == "" ]];then
+
+        echo "Usage: ./nc_port.sh ip "
+        echo "Usage: ./nc_port ip portnumber"
+
+elif [[ $1 != "" && $2 != "" ]];then
+        nc -zv $1 $2 &>/dev/null
+
+
+        if [[ $? == 0 ]];then
+            echo "Port $2 is open"
+        else
+            echo "Port $2 is closed"
+        fi
+else
+        echo "Hit Control-C to stop scanning!" | lolcat
+        for i in {1..65535};do
+        nc -zv $1 $i &>/dev/null
+        if [[ $? == 0 ]];then
+            echo "Port $i is open"
+        fi
+    done
+fi
